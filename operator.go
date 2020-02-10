84f48fae11ab3c07a39ebf18b58c387b2d61f90d
Yudi Andela <yudhi.andhela@gmail.com>
2020-02-10 22:56:04 +0700

    Operator

---
+++ operator.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Operator Aritmatika Dasar
+	fmt.Println(10 + 1) // Penjumlahan
+	fmt.Println(10 - 1) // Pengurangan
+	fmt.Println(10 * 2) // Perkalian
+	fmt.Println(10 / 2) // Pembagian
+	fmt.Println(10 % 3) // Modulo / Sisa Bagi
+
+	// Operator Logika
+	fmt.Println(true && true)  // Benar dan Benar
+	fmt.Println(true && false) // Benar dan Salah
+	fmt.Println(true || true)  // Benar atau Benar
+	fmt.Println(true || false) // Benar atau Salah
+	fmt.Println(!true)         // Tidak Benar
+}
