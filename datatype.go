5b24f414016a96ebfd28b29a7b62cd63d2d36fd1
Yudi Andela <yudhi.andhela@gmail.com>
2020-02-10 22:55:55 +0700

    DataType

---
+++ datatype.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// DataType Numbers
+	fmt.Println(1001)
+	fmt.Println(10.01)
+
+	// DataType String
+	fmt.Println("Yudi Andela")
+	fmt.Println("Madrasah Aliyah")
+
+	// DataType Boolean
+	fmt.Println(true)
+	fmt.Println(false)
+}
