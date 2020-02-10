e24c1dbedc647b9356161a0db0602fa2790af50a
Yudi Andela <yudhi.andhela@gmail.com>
2020-02-11 06:56:40 +0700

    Array

---
+++ array.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [4]string
+
+	names[0] = "Yudi Andela"
+	names[1] = "Marissa Masir"
+	names[2] = "Muhammad Yusza Mahardika"
+	names[3] = "Muhammad Razzaq"
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+
+	fmt.Println("\n")
+
+	/**
+	 * sama dengan Foreach
+	 **/
+	for i, val := range names {
+		fmt.Println("Index", i, "=", val)
+	}
+
+	fmt.Println("\n")
+
+	// Tanpa menggunakan index
+	for _, val := range names {
+		fmt.Println(val)
+	}
+
+}
